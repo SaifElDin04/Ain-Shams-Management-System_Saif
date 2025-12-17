d2cd92b80ff0f6006b3c8d12c2663e79992c491e
Mariam <mariam.gamgoum@gmail.com>
2025-12-17 21:57:11 +0200

    complete assignment backend

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 const authRoutes = require('./routes/authRoutes');
 const courseRoutes = require('./routes/courseRoutes');
+const assignmentRoutes = require('./routes/assignmentRoutes');
 const applicationRoutes = require('./routes/applicationRoutes');
 
 const PORT = process.env.PORT || 4000;
@@ -36,6 +37,7 @@ app.use('/uploads', express.static(UPLOAD_DIR));
 // Mount routes
 app.use('/api/auth', authRoutes);
 app.use('/api/curriculum', courseRoutes);
+app.use('/api/curriculum', assignmentRoutes);
 app.use('/api/applications', applicationRoutes);
 
 app.listen(PORT, () => {
